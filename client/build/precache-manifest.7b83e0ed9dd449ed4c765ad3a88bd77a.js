76a4c67fa42351ac55f4c9aad660ba96c55dfacd
rivendell <webwyler@gmail.com>
2019-11-04 21:44:22 -0600

    changes to json files and build files

---
+++ client/build/precache-manifest.7b83e0ed9dd449ed4c765ad3a88bd77a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d2d1f338842645156d397216d10f5b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6e9e3ce4cb9412891b3",
+    "url": "/static/css/main.09bb5361.chunk.css"
+  },
+  {
+    "revision": "62dff87546d0b82b4223",
+    "url": "/static/js/2.720c1a0d.chunk.js"
+  },
+  {
+    "revision": "e6e9e3ce4cb9412891b3",
+    "url": "/static/js/main.f8463efc.chunk.js"
+  },
+  {
+    "revision": "ad25a987f3a9c706dd48",
+    "url": "/static/js/runtime-main.e653dd2e.js"
+  }
+]);
